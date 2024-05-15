d22fa4d476338b1f2e16beb63847ce2a2133afdd
lunarinaa <aydan.akbarova@gmail.com>
2024-05-15 12:33:53 +0400

    semistandard fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const { argv } = require('process');
 
 fs.readFile(argv[2], 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
