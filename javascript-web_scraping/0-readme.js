181e659b03a93f093a12193e8d4f9ebe320d9dcc
lunarinaa <aydan.akbarova@gmail.com>
2024-05-15 12:27:32 +0400

    readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/opt/homebrew/bin/node
+const fs = require('fs');
+const { argv } = require('process');
+fs.readFile(argv[2], 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+console.log(data);
+});
